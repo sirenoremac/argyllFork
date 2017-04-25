9906339b4e77125804abeb8f38ce8a0a75462e26
ecameron <ericcameron@Eric.local>
2017-04-25 17:33:31 -0500

    Initial commit with one small tweak to skip cal check for i1pro

---
+++ argylltest/argylltest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  argylltest
+//
+//  Created by Eric Cameron on 4/25/17.
+//  Copyright © 2017 FotoKem. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
